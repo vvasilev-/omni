4b5294b26b873abca01136cf477d14e1dd94f487
Viktor Vasilev <liberalcho@gmail.com>
2021-02-10 21:15:49 +0200

    chore: Enable disabled states for background & opacity utils

--- tailwind.config.js
+++ tailwind.config.js
@@ -12,7 +12,10 @@ module.exports = {
         extend: {},
     },
     variants: {
-        extend: {},
+        extend: {
+            backgroundColor: ['disabled'],
+            opacity: ['disabled'],
+        },
     },
     plugins: [],
 };
