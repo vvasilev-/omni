3aeeed42d99a54d95dc24eb7782479840651e373
Viktor Vasilev <liberalcho@gmail.com>
2021-02-12 22:35:37 +0200

    feat(blocks): Use Inertia-based link in sidebar

--- resources/ts/blocks/layout/sidebar-link.tsx
+++ resources/ts/blocks/layout/sidebar-link.tsx
@@ -3,18 +3,20 @@
  */
 import React from 'react';
 import cx from 'classnames';
+import { InertiaLink, InertiaLinkProps } from '@inertiajs/inertia-react';
 
 export type SidebarLinkProps = {
     icon?: React.ReactNode,
     text?: string,
     active?: boolean,
-} & React.AnchorHTMLAttributes<any>;
+} & InertiaLinkProps;
 
 /**
  * @param  {Object} props
  * @param  {React.ReactNode} props.icon
  * @param  {string} props.text
  * @param  {boolean} props.active
+ * @param  {string} props.className
  * @param  {...Object} props.otherProps
  * @return {React.ReactElement}
  */
@@ -22,12 +24,13 @@ export const SidebarLink: React.FunctionComponent<SidebarLinkProps> = ({
     icon,
     text,
     active = false,
+    className,
     ...otherProps
 }) => {
     return (
-        <a
+        <InertiaLink
             className={cx(
-                'flex items-center px-8 py-2 hover:bg-blue-100 hover:text-blue-900 hover:border-r-4 hover:border-blue-900',
+                'flex items-center w-full px-8 py-2 hover:bg-blue-100 hover:text-blue-900 hover:border-r-4 hover:border-blue-900',
                 { 'bg-blue-100 text-blue-900 border-r-4 border-blue-900': active }
             )}
             {...otherProps}
@@ -37,7 +40,7 @@ export const SidebarLink: React.FunctionComponent<SidebarLinkProps> = ({
             {text && (
                 <span className="ml-4 font-medium">{text}</span>
             )}
-        </a>
+        </InertiaLink>
     );
 };
 
